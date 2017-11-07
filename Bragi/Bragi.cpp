4593ff825fb42b06201d0c0e3a1ec09ac3f44919
xorz57 <fot@ceid.upatras.gr>
2017-11-07 02:32:46 +0200

    Added support for visual studio

--- Bragi/Bragi.cpp
+++
@@ -1,8 +0,0 @@
-#include "Bragi.h"
-
-
-int main()
-{
-        std::cout<<"Success\n";
-        return EXIT_SUCCESS;
-}
---
+++ Ragnarok/Bragi/Bragi.cpp
@@ -0,0 +1,8 @@
+#include "Bragi.h"
+#include <iostream>
+
+int main()
+{
+	std::cout<<"Success\n";
+	return EXIT_SUCCESS;
+}
