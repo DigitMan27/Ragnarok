d591606ce30c269c7718bea8b37e74a52b1cb0f9
Ace <adamopoulos@ceid.upatras.gr>
2017-11-08 02:31:25 +0200

    Shows the items in a directory.For now dir is fixed

--- Ragnarok/Bragi/main.cpp
+++ Ragnarok/Bragi/main.cpp
@@ -1,9 +1,16 @@
 #include "Bragi.h"
 #include <iostream>
 
+/*Comments will be posted later*/
 
 int main()
 {
-        std::cout<<"Success\n";
-        return EXIT_SUCCESS;
+	std::vector<std::string> v;
+	v = ListDir("dddd");
+	for (std::vector<std::string>::const_iterator s = v.begin(); s != v.end(); s++)
+	{
+		std::cout << *s << std::endl;
+	}
+	system("pause"); // recommended just to see the result (Windows Start without Debugging runs very fast)
+    return EXIT_SUCCESS;
 }
